613a64b04e238175695e357450ee95f0629a2f11
wellington <wellington.dev89@gmail.com>
2024-03-25 23:14:21 -0300

    Inicializando Repositório

---
+++ app.js
@@ -0,0 +1,7 @@
+function sortear() { 
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade : ${quantidade} \n Do número : ${de} \n Até o número : ${ate}`);
+}
